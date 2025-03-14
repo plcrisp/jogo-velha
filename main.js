f6fa37d4473313893aee4781e9e7d44ff068dc3b
plcrisp <pedrolcrisp@gmail.com>
2025-03-14 17:28:29 -0300

    🐛 fixing winning condition and toggle player

--- main.js
+++ main.js
@@ -7,36 +7,30 @@ var vencedor = '';
 var fimJogo = false;
 var tab = [3];
 
-// Função para verificar vitória
 function verificarVitoria(tab, jogador) {
-    // Verificar as linhas
     for (let i = 0; i < 3; i++) {
         if (tab[i][0] === jogador && tab[i][1] === jogador && tab[i][2] === jogador) {
-            return true; // Vitória horizontal
+            return true;
         }
     }
 
-    // Verificar as colunas
     for (let i = 0; i < 3; i++) {
         if (tab[0][i] === jogador && tab[1][i] === jogador && tab[2][i] === jogador) {
-            return true; // Vitória vertical
+            return true; 
         }
     }
 
-    // Verificar a diagonal principal
     if (tab[0][0] === jogador && tab[1][1] === jogador && tab[2][2] === jogador) {
-        return true; // Vitória diagonal principal
+        return true; 
     }
 
-    // Verificar a diagonal secundária
     if (tab[0][2] === jogador && tab[1][1] === jogador && tab[2][0] === jogador) {
-        return true; // Vitória diagonal secundária
+        return true;
     }
 
-    return false; // Nenhuma vitória
+    return false;
 }
 
-// inicializa o tabuleiro
 for(i = 0; i < 3; i++){
     tab[i] = [3];
     for(j = 0; j < 3; j++){
@@ -54,7 +48,7 @@ while((jogadas < 9) && (fimJogo == false)){
     lin = parseInt(prompt(`${jogador} em [linha]: `));
     col = parseInt(prompt(`${jogador} em [coluna]: `));
     
-    if(!(((lin > 0)&&(lin <= 3 )) && ((col > 0)&&(col <= 3 )))){
+    if(!(((lin >= 0)&&(lin < 3 )) && ((col >= 0)&&(col < 3 )))){
         console.log('A jogada não foi realizada dentro dos limites do tabuleiro.');
         continue;
     } 
@@ -68,8 +62,10 @@ while((jogadas < 9) && (fimJogo == false)){
     jogadas++;
 
     if(!verificarVitoria(tab,jogador)){
+
         if(jogador == 'o') jogador = 'x';
-        if(jogador == 'x') jogador = 'o';
+        else if(jogador == 'x') jogador = 'o';
+
     }else{
         vencedor = jogador;
         fimJogo = true;
